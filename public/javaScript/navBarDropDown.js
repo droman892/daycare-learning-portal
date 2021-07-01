aaa983c8b14058df2afde9ccdf79d10e91bf1efe
droman892 <droman892@gmail.com>
2021-07-01 17:25:44 -0400

    updated

--- public/javaScript/navBarDropDown.js
+++ public/javaScript/navBarDropDown.js
@@ -1,16 +1,8 @@
-/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple
-         * dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
-}
+$('.top-menu').hover(
+    function () {
+        $(this).find('.sub-menu').show("slow");
+    },
+    function () {
+        $(this).find('.sub-menu').hide("slow");
+    }
+);
